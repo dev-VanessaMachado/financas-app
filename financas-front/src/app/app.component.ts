ab4189c9f5a603018664e79aa4e07a1a49168d13
dev-VanessaMachado <dev.vanessamachado@gmail.com>
2026-07-10 10:16:36 -0300

    feat: adiciona exibicao dinamica de saldo do usuario

--- financas-front/src/app/app.component.ts
+++ financas-front/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',               // A tag HTML customizada que representa este componente
+  templateUrl: './app.component.html', // O arquivo que cuida do visual
+  styleUrls: ['./app.component.css']    // O arquivo que cuida dos estilos
 })
 export class AppComponent {
-  title = 'financas-front';
+
+  // =========================================================================
+  // PROPRIEDADES (Atributos da Classe)
+  // =========================================================================
+
+  // Uma variável simples do tipo texto (string)
+  title: string = 'financas-front';
+
+  // Um objeto estruturado para simular os dados consolidados do usuário.
+  // No TypeScript, objetos usam a estrutura de chave: valor entre chaves {}.
+  resumoSaldo = {
+    usuario: 'Vanessa Machado',
+    saldoAtual: 4610.50,
+    status: 'Positivo'
+  };
 }
